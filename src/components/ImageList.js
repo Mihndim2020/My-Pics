2a028ec97bd0fa0aa3d4bab2e303d58ecf6b4e32
Mihndim <mihndim2016@gmail.com>
2023-05-30 06:05:42 +0100

    Add components

---
+++ src/components/ImageList.js
@@ -0,0 +1,9 @@
+const ImageList = () => {
+  return (
+    <div>
+      <h1>Welcome to My Pics ImageList</h1>
+    </div>
+  );
+};
+
+export default ImageList;
---
+++ src/components/ImageShow.js
@@ -0,0 +1,9 @@
+const ImageShow = () => {
+  return (
+    <div>
+      <h1>Welcome to My Pics ImageShow</h1>
+    </div>
+  );
+};
+
+export default ImageShow;
